af0c733ff3108909be3ecff8daebfeca7b9574fd
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-07 08:08:57 -0700

    Add project files.

---
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -0,0 +1,18 @@
+'Joshua Makuch
+'RCET0265
+'Winter 2023
+'Shuffle the deck
+'
+
+Option Explicit On
+Option Strict On
+
+Imports System
+
+Module ShuffleTheDeck
+    Sub Main(args As String())
+
+
+
+    End Sub
+End Module
