596e8e5b90e7b37082a48db9a530bd5229df2ce0
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-07 08:34:25 -0700

    Implemented a function to close it at any user input, also included the "shuffling" that sets all possible values within the drawn cards array to false

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -2,7 +2,7 @@
 'RCET0265
 'Winter 2023
 'Shuffle the deck
-'
+'https://github.com/JoshuaMakuch/ShuffleTheDeck.git
 
 Option Explicit On
 Option Strict On
@@ -10,9 +10,44 @@ Option Strict On
 Imports System
 
 Module ShuffleTheDeck
-    Sub Main(args As String())
+    Sub Main()
 
+        drawAndShuffle()
 
 
     End Sub
+    Function drawAndShuffle() As Boolean
+
+        Dim random As New Random
+        Dim suits As String() = {"Spades", "Clubs", "Hearts", "Diamonds"}
+        Dim values As String() = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
+        Dim drawnCards(3, 12) As Boolean
+        Dim userInput As String
+
+        While True
+
+            Console.WriteLine("Hello user, press 1 to shuffle, press 2 to deal a card, press 3 to quit the program at any time.")
+            userInput = Console.ReadLine()
+
+            Select Case userInput
+
+                Case "1"
+                    For i As Integer = 0 To 3
+                        For j As Integer = 0 To 12
+                            drawnCards(i, j) = False
+                        Next
+                    Next
+                Case "2"
+
+                Case "3"
+                    Return True
+
+            End Select
+
+
+
+        End While
+
+    End Function
+
 End Module
