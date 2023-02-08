5b4f5d3d6181985bd03d453e6e51aad76d0b7336
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-08 08:57:21 -0700

    Fixed the main issue with the random class not picking the upper end of the drawn cards array values (which got caught in a while loop and prevented the drawn cards from ever filling) and made the console look a little better by clearing it and indexing how man cards the user has drawn total.

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -12,41 +12,92 @@ Imports System
 Module ShuffleTheDeck
     Sub Main()
 
-        drawAndShuffle()
-
+        DrawAndShuffle()
+        Console.WriteLine("Goodbye!")
 
     End Sub
-    Function drawAndShuffle() As Boolean
+    Function DrawAndShuffle() As Boolean
 
         Dim random As New Random
         Dim suits As String() = {"Spades", "Clubs", "Hearts", "Diamonds"}
-        Dim values As String() = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
-        Dim drawnCards(3, 12) As Boolean
+        Dim values As String() = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}
         Dim userInput As String
+        Dim randomSuit As Integer
+        Dim randomValue As Integer
+        Dim indexOfTotalDrawn As Integer
+        Dim indexOfRandomDrawn As Integer
+        Dim drawnCards(3, 12) As Boolean
+        Dim allDrawn As Boolean
 
         While True
 
-            Console.WriteLine("Hello user, press 1 to shuffle, press 2 to deal a card, press 3 to quit the program at any time.")
+            Console.WriteLine(vbCrLf & "Hello user, press 1 to shuffle, press 2 to deal a card, press 3 to quit the program at any time.")
             userInput = Console.ReadLine()
+            Console.Clear()
 
-            Select Case userInput
+            If userInput = "1" Or allDrawn Then
 
-                Case "1"
-                    For i As Integer = 0 To 3
-                        For j As Integer = 0 To 12
-                            drawnCards(i, j) = False
-                        Next
+                'When the user chooses to, or when needed, clears the array
+                For i As Integer = 0 To 3
+                    For j As Integer = 0 To 12
+                        drawnCards(i, j) = False
                     Next
-                Case "2"
+                Next
+                allDrawn = False
+                indexOfTotalDrawn = 0
+                Console.WriteLine("Deck has been shuffled..." & vbCrLf & $"{indexOfTotalDrawn} cards drawn")
 
-                Case "3"
-                    Return True
+            ElseIf userInput = "2" Then
 
-            End Select
+                'Generates a random card value
+                randomSuit = random.Next(0, 4)
+                randomValue = random.Next(0, 13)
+
+                'Continuously generates a drawn card until it has picked a card that has not been drawn yet
+                While drawnCards(randomSuit, randomValue)
+
+                    randomSuit = random.Next(0, 4)
+                    randomValue = random.Next(0, 13)
+
+                End While
+
+                'Once a card that hasn't been drawn, is drawn, it sets that card to true and displays what the card drawn is.
+                drawnCards(randomSuit, randomValue) = True
+                Console.WriteLine($"Drawn Card: {values(randomValue)} of {suits(randomSuit)}")
+
+
+                'Immediatly assumes all cards have been drawn
+                allDrawn = True
+                'Checks if all the cards have been drawn after each new card is drawn and exits the for loops if it does find a not drawn card
+                For i As Integer = 0 To 3
+                    For j As Integer = 0 To 12
+                        If drawnCards(i, j) = False Then
+                            allDrawn = False
+                            Exit For
+                        End If
+                    Next
+                    If allDrawn = False Then
+                        Exit For
+                    End If
+                Next
+                'Prints out how many cards have been drawn
+                indexOfTotalDrawn += 1
+                Console.WriteLine($"{indexOfTotalDrawn} cards drawn")
+
+            ElseIf userInput = "3" Then
+
+                'When the user chooses 3 or to quit then the function is ended and the code is finshied.
+                Return True
+
+            Else
+                Console.WriteLine("That's not a proper input...")
+            End If
 
 
 
         End While
+
+        Return True
 
     End Function
 
